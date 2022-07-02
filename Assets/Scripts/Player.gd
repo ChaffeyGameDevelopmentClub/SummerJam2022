dae4316380e58f6f553b61fb81226fcb2680a274
Kamran Haq <KamranHaq@protonmail.com>
2022-07-01 22:49:50 -0700

    Added Animation Player

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -5,9 +5,15 @@ const VELOCITY = 500
 var input_vector: Vector2 = Vector2.ZERO
 var velocity_vector: Vector2 = Vector2.ZERO
 
+onready var AimLine = $AimLine
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+#Handle miscellaneous inputs
+func _input(event): 
+	pass
 
 #Process inputs
 func _process(delta):
@@ -19,4 +25,6 @@ func _process(delta):
 func _physics_process(delta):
 	velocity_vector = input_vector * VELOCITY
 	velocity_vector = move_and_slide(velocity_vector)
+	var angle_to_mouse = self.position.angle_to_point(get_global_mouse_position())
+	AimLine.set_rotation(angle_to_mouse + PI)
 	
