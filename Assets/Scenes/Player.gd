50c3a9aeea9cf9278ec699f719453724f69c5f95
Kamran Haq <KamranHaq@protonmail.com>
2022-07-01 21:57:08 -0700

    Removed Print Statement

--- Assets/Scenes/Player.gd
+++ Assets/Scenes/Player.gd
@@ -14,7 +14,6 @@ func _process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	print(input_vector.length())
 
 #Process physics
 func _physics_process(delta):
