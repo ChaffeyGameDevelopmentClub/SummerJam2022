10a182dcfcc78ca6ea66df8901fdee99ccc88bd4
Kamran Haq <KamranHaq@protonmail.com>
2022-07-01 21:14:54 -0700

    Removed Some Menu Fluff

--- Assets/Scripts/MenuController.gd
+++ Assets/Scripts/MenuController.gd
@@ -7,34 +7,13 @@ func _ready():
 
 # button functionality (changes scene)
 func _on_PlayButton_pressed():
-	get_tree().change_scene("res://Assets/Scenes/Stages/Stages/TestStage_Battle.tscn")
+	#get_tree().change_scene("")
+	pass
 	
-#Launch Multiplayer Menu
-func _on_MultiplayerButton_pressed():
-	get_tree().change_scene("res://Assets/Scenes/Multiplayer/lobby.tscn")
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
 
-# color changes in bg
-func _on_play_button_mouse_entered():
-	BG.target_color = Color.lightpink
-
-func _on_mult_button_mouse_entered():
-	BG.target_color = Color.aqua
-
-func _on_settings_button_mouse_entered():
-	BG.target_color = Color.mediumspringgreen
-
-func _on_credits_button_mouse_entered():
-	BG.target_color = Color.slateblue
-
-func _on_exit_button_mouse_entered():
-	BG.target_color = Color.darkkhaki
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 
