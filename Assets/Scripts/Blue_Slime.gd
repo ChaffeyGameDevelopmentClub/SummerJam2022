c7152cf2f3a8ef7157659349a20860006abbd6b8
jcarmona4254 <n/a>
2022-07-05 16:19:17 -0700

    initial commit
    enemy not working correctly, needs tweaking. doesn't move left to right correctly

---
+++ Assets/Scripts/Blue_Slime.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export var speed = 100
+
+
+# Declare member variables here. Examples:
+var velocity: Vector2 = Vector2.ZERO
+var acceleration: Vector2 = Vector2.ZERO
+
+funct
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
